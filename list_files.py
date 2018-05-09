b225a331ffb381eb5952621344b06a62868a44f4
Henning Schiebenhoefer <henning.schiebenhoefer@posteo.de>
2018-05-09 13:38:26 +0200

    error handling: raise IOError if input path does not exist, raise warning if no files are found

--- list_files.py
+++ list_files.py
@@ -2,7 +2,7 @@ import re
 import os
 
 
-def get_list_of_files(location, file_regex=r"FDR_.*_false_summary_xiFDR(\d+\.)*csv"):
+def get_list_of_files(location, file_regex=r".*/FDR_.*_false_summary_xiFDR(\d+\.)*csv"):
     """
     searches location recursively for files matching regex.
     regex has to match full file path.
@@ -15,12 +15,17 @@ def get_list_of_files(location, file_regex=r"FDR_.*_false_summary_xiFDR(\d+\.)*c
     """
     list_of_files = []
     regex = re.compile(file_regex)
+    if not os.path.exists(location):
+        raise IOError("Specified location '{loc}' does not exist in filesystem.".format(loc=location))
     for rel_dir, sub_dirs, files in os.walk(location):
         for f in files:
             f_full = os.path.join(rel_dir, f)
             # print f_full
             if regex.match(f_full):
                 list_of_files.append(f_full)
+    if len(list_of_files) == 0:
+        raise Warning("No files were found in '{loc}' that match '{re}'"\
+                      .format(loc=location, re=file_regex))
     return sorted(list_of_files)
 
 

4196c90ac5ed9213261e41d146ed11f8a0742062
henning <henning.schiebenhoefer@posteo.de>
2018-08-15 13:41:40 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='xl-search-space-libs',
+      version='0.1',
+      description='Scripts that are required by several pipelines in the search space optimization.',
+      url='http://github.com/rababerladuseladim/xl-search-space-libs',
+      author='Henning',
+      packages=['xl-search-space-libs'],
+      zip_safe=False)

a2b91e47fd8bfe5626c38cbe56838a01e661946d
henning <henning.schiebenhoefer@posteo.de>
2018-08-15 12:58:25 +0200

    swantjes outdated preprocessing pipeline

---
+++ preprocessing_config.py
@@ -0,0 +1,5 @@
+msconvert_exe = 'C:/Program Files/ProteoWizard/ProteoWizard 3.0.9740/msconvert.exe'
+mscon_settings = ['peakPicking true 2-', 'msLevel 2-', 'MS2Denoise 20 100 false', 'titleMaker <RunId>.<ScanNumber>.<ScanNumber>.<ChargeState>']
+split_acq = False
+detector_filter = 'all'
+nthr = 2
